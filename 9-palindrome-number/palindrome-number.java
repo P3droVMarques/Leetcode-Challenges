b64e714c15147e6525236ad02c572d5ef962e4a5
Pedro Marques <165982827+P3droVMarques@users.noreply.github.com>
2026-05-03 21:41:00 -0300

    Time: 5 ms (79.55%) | Memory: 46 MB (34.28%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+
+        int result = 0;
+        int num = x;
+
+        while(num > 0){
+                int digit = num % 10;
+                result = result * 10 + digit;
+                num = num / 10;
+        }
+
+        return (result == x);
+    }
+}
